5e6e93fece10b18f5c3dcb511e243e25d340703b
Kiwian <kiwiandoesthings@gmail.com>
2026-05-02 11:48:52 -0400

    start of the revolution

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-A small NodeJS+SQLite web app that allows for Discord-like messaging and features. It is still heavily under development.
+A small web app that allows for Discord-like messaging and features. It is still heavily under development.
+Uses websockets to recieve data, and uses api.kiwiandoesthings.place to push data.
---
+++ common.js
@@ -0,0 +1,26 @@
+const socket = io();
+
+socket.on("alert_server", (alertMessage) => {
+	alert(alertMessage);
+});
+
+function setCookie(key, value) {
+  const expiry = new Date();
+  expiry.setDate(expiry.getDate() + 365);
+  document.cookie = `${key}=${value}; expires=${expiry.toUTCString()}; path=/`;
+}
+
+function getCookie(cookie) {
+  const cookies = document.cookie.split("; ");
+  for (let c of cookies) {
+    const [key, value] = c.split("=");
+    if (key === cookie) {
+		return value;
+	}
+  }
+  return null;
+}
+
+function colorMsg(message, color = "white") {
+	return `<span style="color: ${color};">${message}</span>`;
+}
